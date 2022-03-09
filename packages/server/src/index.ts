5b808dd206eda841ab0002558e1bb8ac7dd4ccc0
m_khorosh <marina.khoroshilova@gmail.com>
2022-03-09 13:57:13 +0300

    mongo+linters

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,25 @@
-import express from 'express'
+import SessionModel from "../models/Session";
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@cinema-app-cluster.ldyyk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+})
+
 
 const app = express();
-const port = 5000;
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+app.use(express.json());
+app.use(cors());
+const port = 3003;
+app.get('/', async (req, res) => {
+  SessionModel.find({}, (error, result) =>{
+      if(error){
+          res.send(error);
+      }
+      res.send(result);
+  });
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
